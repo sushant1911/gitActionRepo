29fc8a2a21ec426501a5db638d7c816bc01ae2df
sushantsuman <sushantcvrcebtech17@gmail.com>
2024-08-07 16:02:17 +0530

    Initial commit

---
+++ src/main/java/com/sushant/git/actions/GitController.java
@@ -0,0 +1,15 @@
+package com.sushant.git.actions;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class GitController {
+    @GetMapping("/welcome")
+    public String welcome()
+    {
+        return "welcome";
+    }
+}
---
+++ src/test/java/com/sushant/git/actions/GitActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushant.git.actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
