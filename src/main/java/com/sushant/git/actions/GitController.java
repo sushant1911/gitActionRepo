df15408b236b605bc268119cb9e29ffff0bf5b70
sushantsuman <sushantcvrcebtech17@gmail.com>
2024-08-14 13:50:30 +0530

    added new message

--- src/main/java/com/sushant/git/actions/GitController.java
+++ src/main/java/com/sushant/git/actions/GitController.java
@@ -1,5 +1,6 @@
 package com.sushant.git.actions;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,6 @@ public class GitController {
     @GetMapping("/welcome")
     public String welcome()
     {
-        return "welcome again";
+        return "welcome this is";
     }
 }
